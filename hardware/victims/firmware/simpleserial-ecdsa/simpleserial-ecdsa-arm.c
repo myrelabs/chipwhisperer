a0ffa5c229369af6d4f0dda365f6120870a423a1
Przemyslaw Kubiak <przemyslaw.grzegorz.kubiak@gmail.com>
2021-02-16 19:16:52 +0100

    some epsilon changes, signature verification moved to the chipwhisperer python software

--- hardware/victims/firmware/simpleserial-ecdsa/simpleserial-ecdsa-arm.c
+++ hardware/victims/firmware/simpleserial-ecdsa/simpleserial-ecdsa-arm.c
@@ -14,6 +14,8 @@
 
 #define ECPARAMS   MBEDTLS_ECP_DP_BP256R1
 #define FIELD_LEN  32
+#define BASEPOINT_ORDER_LEN  FIELD_LEN
+
 
 static int  key_is_empty;
 mbedtls_ecdsa_context ctx;
@@ -122,18 +124,18 @@ uint8_t ecdsa_gen_sig(uint8_t *pt)   //pt[0] contains the value of the length of
 {
     int      ret = 0;          //longer type than the output type, but the simplesierial_get uses a sigle octet array in ack
     
-    uint8_t  buf_for_sig[2*(1 + FIELD_LEN)];  //bitlength of ord G can be by 1 bit longer than bitlength of the field
+    uint8_t  buf_for_sig[2*(BASEPOINT_ORDER_LEN)]; 
     mbedtls_mpi r, s;
 
     mbedtls_mpi_init( &r );
     mbedtls_mpi_init( &s );
     MBEDTLS_MPI_CHK( mbedtls_ecdsa_sign( &ctx.grp, &r, &s, &ctx.d, pt+1, pt[0], myrand, NULL ) );
-    MBEDTLS_MPI_CHK( mbedtls_ecdsa_verify( &ctx.grp, pt+1, pt[0], &ctx.Q, &r, &s ) );
+    //MBEDTLS_MPI_CHK( mbedtls_ecdsa_verify( &ctx.grp, pt+1, pt[0], &ctx.Q, &r, &s ) );
 
-    memset(buf_for_sig, 0, 2*(1 + FIELD_LEN));
-    MBEDTLS_MPI_CHK( mbedtls_mpi_write_binary( &r, buf_for_sig, 1 + FIELD_LEN ) );
-    MBEDTLS_MPI_CHK( mbedtls_mpi_write_binary( &s, buf_for_sig + (1 + FIELD_LEN), 1 + FIELD_LEN ) );
-    simpleserial_put('r', 2*(1 + FIELD_LEN), buf_for_sig);
+    memset(buf_for_sig, 0, 2*(BASEPOINT_ORDER_LEN));
+    MBEDTLS_MPI_CHK( mbedtls_mpi_write_binary( &r, buf_for_sig, BASEPOINT_ORDER_LEN ) );
+    MBEDTLS_MPI_CHK( mbedtls_mpi_write_binary( &s, buf_for_sig + BASEPOINT_ORDER_LEN, BASEPOINT_ORDER_LEN ) );
+    simpleserial_put('r', 2*(BASEPOINT_ORDER_LEN), buf_for_sig);
 
 cleanup:
     if (ret) simpleserial_put('r', sizeof(int), (uint8_t *)&ret);
